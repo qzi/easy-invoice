c2ebc74dd6a7f7d8a7f438679b5882b616bea259
Leon Qiu <leon.qiu@agilelog.org>
2024-09-13 19:35:34 +0800

    easy invoice init

---
+++ src/components/InvoiceItem.mjs
@@ -0,0 +1,28 @@
+import React from "react";
+
+function InvoiceItem({ item, index, handleItemChange }) {
+  return (
+    <div style={{ display: "flex", marginBottom: "10px" }}>
+      <input
+        type="text"
+        placeholder="Item Description"
+        value={item.description}
+        onChange={(e) => handleItemChange(index, "description", e.target.value)}
+        style={{ marginRight: "10px", width: "200px" }}
+        className="invoice-input"
+      />
+
+      <input
+        type="number"
+        placeholder="Price"
+        value={item.price}
+        onChange={(e) => handleItemChange(index, "price", e.target.value)}
+        style={{ marginRight: "10px" }}
+        className="invoice-input"
+      />
+      {/* <span>{(item.quantity * item.price).toFixed(2)}</span> */}
+    </div>
+  );
+}
+
+export default InvoiceItem;
